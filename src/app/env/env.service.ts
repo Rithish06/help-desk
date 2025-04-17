84aa7a208df2814bb1475a115cddd12ad5b1405a
keerthana <keerthana.m@iamneo.ai>
2025-04-17 13:33:04 +0530

    aws-deploy-env

--- src/app/env/env.service.ts
+++ src/app/env/env.service.ts
@@ -4,5 +4,8 @@
 // export const apiUrl = "https://helpdesk-demo-812956739285.asia-south1.run.app/api/v1"
 // export const loginInUrl = "https://helpdesk-demo-812956739285.asia-south1.run.app"
 
-export const apiUrl = "http://52.66.89.121:4000/api/v1"
-export const loginInUrl = "http://52.66.89.121:4000"
+// export const apiUrl = "http://52.66.89.121:4000/api/v1"
+// export const loginInUrl = "http://52.66.89.121:4000"
+
+export const apiUrl = "http://api.hroproindia.in/api/v1"
+export const loginInUrl = "http://api.hroproindia.in"
